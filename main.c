af11ae8dd7e274329dd223c9cd4c2be8aa84731f
Brian Wright <wright@is.mpg.de>
2018-10-29 15:00:40 +0100

    update continuously and stream to serial usb at 8khz, as requested

--- main.c
+++ main.c
@@ -3,8 +3,13 @@
 #include "m_spi.h"
 #include "mpu9250.h"
 
-uint8_t _buffer[21];
-int16_t data[10];
+void setup_timer();
+
+volatile int16_t stream = 0;
+volatile uint32_t time = 0;
+uint8_t _buffer[21] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int16_t data[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+uint32_t *time_ptr = (uint32_t*)&data;
 
 // TODO-lo: Transform accel and gyro to match magnetometer (see bolderflight/mpu9250.h:189)
 
@@ -14,16 +19,62 @@ int main()
   m_mpu9250_init();
   m_mpu9250_fast_mode();
   m_usb_init();
+  setup_timer();
 
   for(;;)
   {
-    uint8_t i;
+    uint8_t i, incoming;
 
+    // Continuously read IMU data into the data vector
     m_read_spi_registers(ACCEL_OUT, 21, _buffer);
-    for (i = 0; i <  7; i++)  { data[i] = (((int16_t)_buffer[2*i]) << 8) | _buffer[2*i+1]; }
-    for (i = 7; i < 10; i++)  { data[i] = (((int16_t)_buffer[2*i+1]) << 8) | _buffer[2*i]; }
+    for (i = 0; i <  7; i++)  { data[i+2] = (((int16_t)_buffer[2*i]) << 8) | _buffer[2*i+1]; }
+    for (i = 7; i < 10; i++)  { data[i+2] = (((int16_t)_buffer[2*i+1]) << 8) | _buffer[2*i]; }
 
-    for (i = 0; i < 10; i++)  { write_int16_to_usb(data[i]); }
-    write_newline_to_usb();
+    if (m_usb_rx_available())
+    {
+      incoming = m_usb_rx_char();
+      switch (incoming)
+      {
+        case 'G':
+          stream = ON;
+          m_green(ON);
+          break;
+        case 'S':
+          stream = OFF;
+          m_green(OFF);
+          break;
+        default:
+          break;
+      }
+    }
   }
 }
+
+
+void setup_timer()
+{
+  OCR1A = 2000;  // 8 kHz
+  set(TCCR1B, WGM13);  // PWM (up to OCR1A)
+  set(TCCR1B, WGM12);
+  set(TCCR1A, WGM11);
+  set(TCCR1A, WGM10);
+  set(TIMSK1, OCIE1A);  // Interrupt when OCR1A
+  set(TCCR1B, CS10);  // Div 1
+  set(DDRB,6);
+  OCR1B = 1000;
+  set(TCCR1A,COM1B1);  // clear at B, set at A (to verify speed using B6)
+  sei();
+}
+
+
+ISR(TIMER1_COMPA_vect)
+{
+  if (stream)
+  {
+    uint8_t i;
+    for (i = 0; i < 12; i++)  { write_int16_to_usb(data[i]); }
+    write_newline_to_usb();
+    (*time_ptr)++;
+  }
+}
+
